13686e2f1b7a73335cd620873d31ab3c8dba38b9
Daniel <dynielsen1@hotmail.com>
2018-06-20 16:18:41 +0200

    update

--- information.py
+++
@@ -1,8 +0,0 @@
-#Information
-#calculate salaries
-salary_monthly_NL_EURO=2850
-salary_monthly_USA_EURO=4911.833333
-
-salaries=[salary_monthly_NL_EURO,salary_monthly_USA_EURO]
-
-usa_delta=(salaries[1]-salaries[0])/salaries[0]
